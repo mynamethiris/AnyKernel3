d7525e55ed840a963d06fc10f51da0a743bf4c5d
Thiris <98958343+mynamethiris@users.noreply.github.com>
2025-03-10 00:35:12 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=auto;
+block=/dev/block/bootdevice/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
@@ -39,6 +39,47 @@ PATCH_VBMETA_FLAG=auto;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+# init.rc
+backup_file init.rc;
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+# init.tuna.rc
+backup_file init.tuna.rc;
+insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+append_file init.tuna.rc "bootscript" init.tuna;
+
+# fstab.tuna
+backup_file fstab.tuna;
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+append_file fstab.tuna "usbdisk" fstab;
+
+# Check if boot img has Magisk Patched
+cd $split_img;
+if [ ! "$magisk_patched" ]; then
+  $bin/magiskboot cpio ramdisk.cpio test;
+  magisk_patched=$?;
+fi;
+if [ $((magisk_patched & 3)) -eq 1 ]; then
+  ui_print "! Magisk Detected, U don't need to reinstall Magisk !";
+  WITHMAGISK=Y
+fi;
+cd $home
+
+# Set SELinux to enforcing
+patch_cmdline androidboot.selinux androidboot.selinux=enforcing
+
+# Put Android Version on cmdline
+android_ver=$(file_getprop /system/build.prop ro.build.version.release);
+patch_cmdline androidboot.version androidboot.version=$android_ver
+
+# Set charging speed to stock or mode 0
+patch_cmdline ffc ffc=0
+
+# KernelSU Support
+patch_cmdline kernelsu.enabled kernelsu.enabled=1
 
 # write install
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
